f5f388bb75e112ce3e0114ea6b8104df438c8425
Guojian Zhou <guojian.zhou@windriver.com>
2017-04-20 03:45:05 -0400

    mbp: Add the BSP_NAME as the TOPDIR function
    When it could not find BSP_NAME from command line, then it
    will create use the default parent directroy as the BSP_NAME,
    and create the whole MBP tree in this directory.
    Or user input the BSP_NAME with "-n", then it will create a
    new directory in the directory, then create the whole MBP tree
    in the new "BSP_NAME" direcotry.
    
    Signed-off-by: Guojian Zhou <guojian.zhou@windriver.com>

--- tools/mbp_submodule_create.sh
+++ tools/mbp_submodule_create.sh
@@ -2,9 +2,11 @@ branch="master"
 
 cd ../
 TOP_DIR=`pwd`
-BSP_NAME=`basename $TOP_DIR`
+BSP_NAME_DEFAULT=`basename $TOP_DIR`
+BSP_NAME=""
 BRANCH="master"
 MBP_SCRIPTS_URL="git://pek-lpdfs01.wrs.com/managed_builds/Pulsar/MBP/New/mbp-scripts.git"
+SMARTPM_SECURE_URL="git://pek-lpdfs01.wrs.com/managed_builds/Pulsar/SRC/Pulsar8/meta-smartpm-secure"
 
 usage() {
     echo >&2 "usage: ${0##*/} [-b <#branch> ] [-n <#bsp-name>] [-h] [?] "
@@ -24,6 +26,25 @@ while getopts "b:n:?h" FLAG; do
 done
 
 shift $((OPTIND - 1))
+
+
+function prepare_creste_git()
+{
+    if [ X"$BSP_NAME" != X"$BSP_NAME_DEFAULT" ]; then
+        echo "Create the whole MBP in $BSP_NAME directory!"
+        if [ -d $BSP_NAME ]; then
+            echo "$BSP_NAME directory exist!"
+            mv $BSP_NAME $BSP_NAME.bak
+        else
+            echo "Create the new $BSP_NAME"
+            mkdir $BSP_NAME
+        fi
+    fi
+    cd $TOP_DIR/mbp
+    MBP_URL="`git remote get-url origin`"
+    cd $TOP_DIR/$BSP_NAME
+    git clone $MBP_URL
+}
 
 function clone_extern_git()
 {
@@ -63,7 +84,7 @@ function bare_git_create ()
         git init --bare $each
         git --git-dir=$each config core.sharedRepository all
         find "$each" -type d -print0 | xargs -0 chmod g+ws
-        git commit -s -m "pulsar: $BSP_NAME $each Initial Commit"
+        #git commit -s -m "pulsar: $BSP_NAME $each Initial Commit"
     done
 }
 
@@ -87,7 +108,7 @@ submodule_git()
 {
     cd $TOP_DIR/mbp
     git submodule init --bare
-    for each in buildhistory deploy sstate-cache tmp prdb downloads src meta-smartpm-secure; do
+    for each in buildhistory deploy sstate-cache tmp prdb downloads src; do
        if [ ! -e $each/.git ]; then
            git submodule add ../${BSP_NAME}/$each
        fi
@@ -98,9 +119,21 @@ submodule_git()
     #cat .gitmodules
     git submodule add $MBP_SCRIPTS_URL scripts
     rm -fr scripts
+    git submodule add $SMARTPM_SECURE_URL meta-smartpm-secure
+    rm -fr meta-smartpm-secure
 }
 
-clone_extern_git
+if [ X"$BSP_NAME" != X"" -a X"$BSP_NAME" != X"$BSP_NAME_DEFAULT" ];then
+    prepare_creste_git
+    cd $TOP_DIR/$BSP_NAME
+    TOP_DIR=`pwd`
+fi
+
+if [ X"$BSP_NAME" == X"" ]; then
+    BSP_NAME=$BSP_NAME_DEFAULT
+fi
+
+#clone_extern_git
 bare_git_create
 bare_git_init_create
 
